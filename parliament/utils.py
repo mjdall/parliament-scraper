2f0b410b18bccf898c44fce6c7afa1d4ff348fac
mjdall <morgandally4@gmail.com>
2021-07-29 08:25:08 +1200

    Implement Report Structure Parser

--- parliament/utils.py
+++ parliament/utils.py
@@ -16,3 +16,14 @@ def save_json(json_structure, outfilename):
 
     with open(outfilename, "w", encoding="utf-8") as f:
         json.dump(json_structure, f, indent=2, sort_keys=True)
+
+
+def load_json(filename):
+    if not os.path.exists(filename):
+        raise FileNotFoundError(f"could not find: `{filename}`...")
+
+    loaded_json = None
+    with open(filename, "r", encoding="utf-8") as f:
+        loaded_json = json.load(f)
+    
+    return loaded_json
